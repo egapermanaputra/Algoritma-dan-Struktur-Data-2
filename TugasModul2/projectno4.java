2e4727bf89a72bd742399b32170ec6bc0d633e18
egapermanaputra <80741200+egapermanaputra@users.noreply.github.com>
2021-04-05 15:01:32 +0700

    Add files via upload

---
+++ TugasModul2/projectno4.java
@@ -0,0 +1,24 @@
+package tugasmodul02;
+public class projectno4 {
+    public static void main(String[] args) {
+        int [][] A = {
+            {2,1,-5},
+            {3,4,2}
+        };
+        int tranpose [][] = new int[3][2];
+        for (int i=0; i<A.length; i++){
+            for (int j=0; j<A[0].length; j++){
+                tranpose[j][i] = A[i][j];
+            }
+        }
+        
+        for (int i=0; i<A[0].length; i++){
+            for (int j=0; j<A.length; j++){
+                System.out.print(tranpose[i][j]+" ");
+            }
+            System.out.println();
+            System.out.println("Ega Permana Putra");
+    
+}
+    }
+}
